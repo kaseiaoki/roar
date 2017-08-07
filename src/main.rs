4bd4fb59b2bedc752084658b0a81d0d4fe272c30
kaseiaoki <aoki.11235813@ezweb.ne.jp>
2017-08-08 02:01:53 +0900

    beta

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use std::io;
+use std::string::String;
+use std::io::prelude::*;
+use std::env;
+use std::vec::Vec;
+use std::thread;
+use std::process;
+use std::mem;
+use rand::Rng;
+extern crate rand;
+fn lop(rng: u32) {
+    let r = rng.clone();
+    thread::spawn(move || roar(r));
+    thread::sleep_ms(3);
+}
+fn roar(rng: u32) {
+    let Em = [
+        "１",
+        "２",
+        "３",
+        "４",
+        "５",
+        "６",
+        "８",
+        "７",
+        "９",
+        "０",
+    ];
+
+    let mut rng = rand::thread_rng().gen_range(1, 71);
+    print!("{}{}", rng, Em[rng % 10]);
+    match rng {
+        rng if rng % 2 == 0 => print!("{}", rng),
+        rng if rng % 3 == 0 => print!("{}", Em[rng % 10]),
+        rng if rng % 5 == 0 => print!("{}", Em[rng % 10]),
+        _ => print!("{}", Em[rng % 10]),
+    }
+}
+
+fn main() {
+    loop {
+        lop(1);
+    }
+}
